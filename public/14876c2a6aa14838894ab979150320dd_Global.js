a7c2339cd8a97da8ef07c34ff58b121098246578
Krishna Goswami <goswamikrishna841@gmail.com>
2024-05-23 19:33:35 +0530

    tcString

--- public/14876c2a6aa14838894ab979150320dd_Global.js
+++ public/14876c2a6aa14838894ab979150320dd_Global.js
@@ -52,7 +52,8 @@ const updateTCString = (setOnlyEventStatus = undefined) => {
       return;
     }
     gvl.readyPromise.then(() => {
-      const encodedTCString = TCString.encode(tcModel);
+    console.log(tcModel);
+     const encodedTCString = TCString.encode(tcModel);
       document.cookie = `preferences_tcf=${encodedTCString}; expires=${new Date(new Date().getTime() + 31536000000).toGMTString()}; SameSite=Lax; path=/`;
       privadoCmpApi.update(encodedTCString, false);
       currentTcString = encodedTCString;
